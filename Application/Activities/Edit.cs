5d7bb6d71cae705d9b5a9dee8e13125e1e6cdc00
Ehsan <s.ehsan.babaei@gmail.com>
2021-08-13 00:15:39 +0430

    EO section 10

--- Application/Activities/Edit.cs
+++ Application/Activities/Edit.cs
@@ -4,17 +4,28 @@ using System.Threading.Tasks;
 using MediatR;
 using Persistence;
 using AutoMapper;
+using FluentValidation;
+using Application.Core;
 
 namespace Application.Activities
 {
     public class Edit
     {
-        public class Command : IRequest
+        public class Command : IRequest<Result<Unit>>
         {
             public Activity Activity { get; set; }
         }
 
-        public class Handler : IRequestHandler<Command>
+        public class CommandValidator : AbstractValidator<Command>
+        {
+            public CommandValidator()
+            {
+                RuleFor(a => a.Activity).SetValidator(new ActivityValidator());
+            }
+        }
+
+
+        public class Handler : IRequestHandler<Command, Result<Unit>>
         {
             private readonly DataContext _context;
             private readonly IMapper _mapper;
@@ -25,12 +36,14 @@ namespace Application.Activities
 
             }
 
-            public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
+            public async Task<Result<Unit>> Handle(Command request, CancellationToken cancellationToken)
             {
                 var activity = await _context.Activities.FindAsync(request.Activity.Id);
+                if (activity == null) return null;
                 _mapper.Map(request.Activity, activity);
-                await _context.SaveChangesAsync();
-                return Unit.Value;
+                var result = await _context.SaveChangesAsync() > 0;
+                if (!result) return Result<Unit>.Failure("Failed to update the activity");
+                return Result<Unit>.Success(Unit.Value);
             }
         }
     }
