2b8f29070af49099ac5032236b87597eb0820e49
Ehsan <s.ehsan.babaei@gmail.com>
2021-08-20 17:32:37 +0430

    EO section 20

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -95,6 +95,10 @@ const Profiles = {
   deletePhoto: (id: string) => request.del(`/photos/${id}`),
   editProfile: (profile: Partial<Profile>) =>
     request.put<void>("/profiles", profile),
+  updateFollowing: (username: string) =>
+    request.post(`/follow/${username}`, {}),
+  listFollowings: (username: string, predicate: string) =>
+    request.get<Profile[]>(`/follow/${username}?predicate=${predicate}`),
 };
 
 const Account = {
